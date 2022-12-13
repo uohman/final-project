3499031e18e5d04d5ce0da7806ee8ecb5e1f9826
uohman <ulrikalovisa@gmail.com>
2022-12-13 11:10:24 +0200

    Set up with basic components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StartPage } from 'components/StartPage/StartPage'
+
+export const Main = () => {
+  return (
+    <div>
+      <StartPage />
+    </div>
+  );
+}
---
+++ src/components/StartPage/StartPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const StartPage = () => {
+  return (
+    <div>
+      <h1>This is StreetSmart! Where are we headed?</h1>
+    </div>
+  );
+}
+
