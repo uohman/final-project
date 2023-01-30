e4a949050aca6bf1e3fceeeebd39686bb00d42d1
uohman <ulrikalovisa@gmail.com>
2023-01-30 10:31:15 +0200

    Fetching clue explanations from backend instead of storing them in frontend

--- src/components/ClueExplanation/ClueExplanation.jsx
+++ src/components/ClueExplanation/ClueExplanation.jsx
@@ -1,32 +1,36 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ClueExplanationWrapper, ClueExplanationContainer, ClueExplanationSubheading, ClueExplanationParagraph } from './ClueExplanation.Styles'
 
 export const ClueExplanation = () => {
+  const [games, setGames] = useState([])
+
+  //* Fetching clue explanations
+  useEffect(() => {
+    fetch('https://final-project-api-veooltntuq-lz.a.run.app/games')
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => {
+        setGames(response.games)
+      })
+  }, [games]);
+
   return (
     <ClueExplanationWrapper>
       <ClueExplanationContainer>
         <ClueExplanationSubheading>5 points</ClueExplanationSubheading>
-        <ClueExplanationParagraph>
-      “Day or night, you can always drive here with your shades on. Just be careful not to get pulled over.” Cult classic The Blues Brothers (1980) was set in Chicago and brothers Jake and Elwood famously wore sunglasses at night while evading authorities.
-        </ClueExplanationParagraph>
+        <ClueExplanationParagraph>{games.length ? games[0].clueExplanation : ''} </ClueExplanationParagraph>
         <ClueExplanationSubheading>4 points</ClueExplanationSubheading>
-        <ClueExplanationParagraph>
-      “The first skyscraper in the world was built here in 1885. Don’t be green with envy if you don’t know the answer just yet.” The skyscraper was the Home Insurance Building, standing 138 feet or 42 meters tall. The Chicago River is dyed green every year on St. Patrick’s Day.
-        </ClueExplanationParagraph>
+        <ClueExplanationParagraph>{games.length ? games[1].clueExplanation : ''} </ClueExplanationParagraph>
         <ClueExplanationSubheading>3 points</ClueExplanationSubheading>
-        <ClueExplanationParagraph>
-      “Even though this place is mainly known for its sports teams, you might be able to see a musical or an opera here too.” We are of course referring to the musical Chicago — the longest-running American musical in Broadway history. And while there is an Opera Theater in Chicago we were mainly thinking of the Oprah Winfrey Show which was shot in Chicago. “Over here you’ll find one of the busiest airports in North America.” Over here, O’Hare. See what we did there? With over 200 non-stop destinations O’Hare Airport in Chicago is considered the world’s most connected airport.
-        </ClueExplanationParagraph>
+        <ClueExplanationParagraph>{games.length ? games[2].clueExplanation : ''} </ClueExplanationParagraph>
         <ClueExplanationSubheading>2 points</ClueExplanationSubheading>
-        <ClueExplanationParagraph>
-      “We’re almost home in the third largest city in the US. Home, sweet home... No, we’re not in Alabama.” Sweet Home Chicago is a blues classic from the 1930s that even Barack Obama sang once at the White House.
-        </ClueExplanationParagraph>
+        <ClueExplanationParagraph>{games.length ? games[3].clueExplanation : ''} </ClueExplanationParagraph>
         <ClueExplanationSubheading>1 point</ClueExplanationSubheading>
-        <ClueExplanationParagraph>
-      “Is it hard to stay chic in this windy city? Yes, but we’ll have a go at it.” The city of Chicago has been known by many nicknames, but it is most widely recognized as the “Windy City”.
-        </ClueExplanationParagraph>
+        <ClueExplanationParagraph>{games.length ? games[4].clueExplanation : ''} </ClueExplanationParagraph>
       </ClueExplanationContainer>
     </ClueExplanationWrapper>
   );
